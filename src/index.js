bd6de56d8968980f8f161cb13186681cf16b2ef8
jimmy_anaya <jimmyanaya2usp@gmail.com>
2022-01-18 08:35:39 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider 
+      domain='dev-a3dutqx1.us.auth0.com' 
+      clientId='LrKeEqJEhUkpeQ7bjcdApG111SrwFlzX'
+      redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
